21f1b73c593f77943027c23d9706f2ac007dbc0c
Chad Larson <techchad2@gmail.com>
2020-08-01 00:42:47 -0500

    add cli and option to set password length

--- randpass.py
+++ randpass.py
@@ -1,6 +1,9 @@
+import argparse
 from random import choice
 
 from typing import List
+from typing import Optional
+from typing import Sequence
 
 UPPER_LETTERS = (x for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 LOWER_LETTERS = (x for x in "abcdefghijklmnopqrstuvwxyz")
@@ -8,12 +11,12 @@ DIGITS = (x for x in "0123456789")
 SPECIAL = (x for x in ".,*&^%$#@!-+=/_:;")
 
 
-def generate_password(possible: List[str]) -> str:
+def generate_password(possible: List[str], length: int) -> str:
     """ Generate one random password. """
     num = lower = upper = False
     while True:
         password = ""
-        for _ in range(10):
+        for _ in range(length):
             rand_char = choice(possible)
             # Make sure that at least one of each is used.
             if rand_char.isdigit():
@@ -36,9 +39,18 @@ def make_possible() -> List[str]:
     return possible
 
 
-def main() -> int:
+def argument_parsing(args: Optional[Sequence[str]] = None) -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--length", type=int, default=10,
+                        help="Length of the password (default 10)")
+
+    return parser.parse_args(args)
+
+
+def main(args: Optional[Sequence[str]] = None) -> int:
+    argv = argument_parsing(args)
     possible = make_possible()
-    password = generate_password(possible)
+    password = generate_password(possible, argv.length)
     print(password)
     return 0
 
