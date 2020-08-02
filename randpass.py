e01c5ced0443d989dd7f703d0d6e862093ee73ad
Chad Larson <techchad2@gmail.com>
2020-08-01 19:13:06 -0500

    add option to use special char and rework generate password

--- randpass.py
+++ randpass.py
@@ -1,5 +1,6 @@
 import argparse
 from random import choice
+from random import shuffle
 
 from typing import List
 from typing import Optional
@@ -8,27 +9,33 @@ from typing import Sequence
 UPPER_LETTERS = (x for x in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 LOWER_LETTERS = (x for x in "abcdefghijklmnopqrstuvwxyz")
 DIGITS = (x for x in "0123456789")
-SPECIAL = (x for x in ".,*&^%$#@!-+=/_:;")
+SPECIAL = [x for x in ".,*&^%$#@!-+=/_:;"]
 
 
-def generate_password(possible: List[str], length: int) -> str:
+def generate_password(possible: List[str], length: int, special: int) -> str:
     """ Generate one random password. """
-    num = lower = upper = False
+    password = []
+    if special:
+        length -= special
+        for x in range(special):
+            password.append(choice(SPECIAL))
     while True:
-        password = ""
-        for _ in range(length):
+        num = lower = upper = False  # making sure one of each
+        characters = []
+        for x in range(length):
             rand_char = choice(possible)
-            # Make sure that at least one of each is used.
             if rand_char.isdigit():
                 num = True
-            elif rand_char.islower():
-                lower = True
             elif rand_char.isupper():
                 upper = True
-            password += rand_char
-        if num is True and lower is True and upper is True:
+            elif rand_char.islower():
+                lower = True
+            characters.append(rand_char)
+        if num and lower and upper:
             break
-    return password
+    password.extend(characters)
+    shuffle(password)
+    return "".join(password)
 
 
 def make_possible() -> List[str]:
@@ -43,6 +50,8 @@ def argument_parsing(args: Optional[Sequence[str]] = None) -> argparse.Namespace
     parser = argparse.ArgumentParser()
     parser.add_argument("-l", "--length", type=int, default=10,
                         help="Length of the password (default 10)")
+    parser.add_argument("-s", "--special", type=int, default=0,
+                        help="Number of special char to use.")
 
     return parser.parse_args(args)
 
@@ -50,7 +59,7 @@ def argument_parsing(args: Optional[Sequence[str]] = None) -> argparse.Namespace
 def main(args: Optional[Sequence[str]] = None) -> int:
     argv = argument_parsing(args)
     possible = make_possible()
-    password = generate_password(possible, argv.length)
+    password = generate_password(possible, argv.length, argv.special)
     print(password)
     return 0
 
